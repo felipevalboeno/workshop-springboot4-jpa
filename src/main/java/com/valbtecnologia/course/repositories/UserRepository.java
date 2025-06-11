d7757e31f80ab08edcfdd73a27b02d161b9611e4
malchik <felipevalboeno@hotmail.com>
2025-06-11 10:26:34 -0300

    Service layer, component registration

--- src/main/java/com/valbtecnologia/course/repositories/UserRepository.java
+++ src/main/java/com/valbtecnologia/course/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.valbtecnologia.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.valbtecnologia.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {

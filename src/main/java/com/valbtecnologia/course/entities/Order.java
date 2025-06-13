bc06b3f7bd23869f72f8044385108c7ee2b5e295
malchik <felipevalboeno@hotmail.com>
2025-06-13 11:06:42 -0300

    OrderStatus enum

--- src/main/java/com/valbtecnologia/course/entities/Order.java
+++ src/main/java/com/valbtecnologia/course/entities/Order.java
@@ -3,6 +3,8 @@ package com.valbtecnologia.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.valbtecnologia.course.entities.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +22,8 @@ public class Order implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
+	
+	private Integer orderStatus;
 
 	
 	@ManyToOne
@@ -29,15 +33,26 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) { 
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);
 	}
 
 	public Long getId() {
 		return id;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public void setId(Long id) {

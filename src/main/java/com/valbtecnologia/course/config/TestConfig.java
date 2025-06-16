5043ed32e3721fc243aba8f62c2bfc218f1cbd5c
FelipeValboeno <felipevalboeno@hotmail.com>
2025-06-16 08:44:29 -0300

    Payment, one-to-one association

--- src/main/java/com/valbtecnologia/course/config/TestConfig.java
+++ src/main/java/com/valbtecnologia/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.valbtecnologia.course.entities.Category;
 import com.valbtecnologia.course.entities.Order;
 import com.valbtecnologia.course.entities.OrderItem;
+import com.valbtecnologia.course.entities.Payment;
 import com.valbtecnologia.course.entities.Product;
 import com.valbtecnologia.course.entities.User;
 import com.valbtecnologia.course.entities.enums.OrderStatus;
@@ -93,6 +94,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 		
 	}
 }
